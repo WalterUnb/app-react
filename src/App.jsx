6aed5ea68adb04fecbf7c2e5cad9e65501f83905
walterunbria <walter.unbria@tcs.com.ve>
2025-08-11 23:19:13 -0400

    Avance 1

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,60 @@ import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
 import Button from './components/Button/Button.jsx'
 import styles from './App.module.sass'
+import photo from './assets/images/family.png'
 
 export default function App() {
   return (
     <div className={styles.app}>
       <Header />
 
-      <main className={styles.container}>
-        <section>
-          <h1>Hola React</h1>
-          <p>Este es tu layout básico listo para construir.</p>
+      <main className="row">
+        <div className={`col-lg-6 col-md-6 col-sm-6 col-6 mt-4 ${styles.display}`}>
+          <img className={styles.photo} src={photo} alt="Family"/>
+        </div>
 
-          <div style={{ marginTop: '1rem' }}>
-            <Button onClick={() => alert('Click!')}>Botón normal</Button>
-            <Button variant="primary" style={{ marginLeft: '0.5rem' }}>
-              Botón primario
-            </Button>
+        <div className={`col-lg-6 col-md-6 col-sm-6 col-6 mt-4 ${styles.display2}`}>
+          <div className='row'>
+            <div className={`col-lg-6 col-md-6 col-sm-6 col-6`}>
+              <h5 className={styles.fondo}>Seguro Salud Flexible</h5>
+            </div>
+            <div className={`col-lg-12 col-md-12 col-sm-12 col-12 mt-4`}>
+              <h1 className={styles.title}>Creado para ti y tu
+                <br />
+                familia</h1>
+            </div>
+            <div className={`col-lg-12 col-md-12 col-sm-12 col-12 mt-4`}>
+              <h6>Tú eliges cuánto pagar. Ingresa tus datos, cotiza y <br /> recibe nuestra asesoría. 100% online.</h6>
+            </div>
+            <div className={`col-lg-12 col-md-12 col-sm-12 col-12 mt-4`}>
+              <div className="input-group w-100 mb-3">
+                <div className="dropdown w-30">
+                  <button
+                    className="btn btn-outline-secondary dropdown-toggle w-100"
+                    type="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Seleccione documento
+                  </button>
+                  <ul className="dropdown-menu">
+                    <li><button className="dropdown-item" type="button">DNI</button></li>
+                    <li><button className="dropdown-item" type="button">Pasaporte</button></li>
+                  </ul>
+                </div>
+                <input
+                  type="text"
+                  className="form-control w-70"
+                  aria-label="Text input with dropdown button"
+                  id="documentInput"
+                  placeholder="Nro. de documento"
+                />
+                {/* <label htmlFor="documentInput">Nro. de documento</label> */}
+              </div>
+            </div>
           </div>
-        </section>
+        </div>
+
       </main>
 
       <Footer />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,23 @@
 import styles from './Header.module.sass'
 import logo from '../assets/images/RIMAC.png'
+import phone from '../assets/images/phone.png'
 
 export default function Header() {
   return (
-    <header className={styles.header}>
-      <div><img className={styles.logo} src={logo} alt="Rimac Logo"/></div>
-
-      <nav className={styles.nav}>
-        <a className={styles.link} href="#">
-          ¡Compra por este medio!
-        </a>
-        <a className={styles.link} href="#">
-          Acerca
-        </a>
-      </nav>
+    <header className={`row ${styles.header}`}>
+        <div className="col-lg-6 col-md-6 col-sm-6 col-6">
+          <div>
+            <img className={styles.logo} src={logo} alt="Rimac Logo" />
+          </div>
+        </div>
+        <div className={`col-lg-6 col-md-6 col-sm-6 col-6 ${styles.alignRight}`}>
+          <nav className={styles.nav}>
+            <a className={styles.link} href="#">
+              ¡Compra por este medio!
+            </a>
+            <a className={styles.number} href="#"><img className={styles.phone} src={phone} alt="Phone Icon" />  (01) 411 6001</a>
+          </nav>
+        </div>
     </header>
-  )
+  );
 }
